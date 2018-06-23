8651af0a84cbaa1119767aa3dfc4277d0c7d2da1
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-06-23 19:21:37 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -3,5 +3,15 @@
 def least_coins(cents)
 
 #Code your answer here!
+quarters = cents / 25
+  dimes = cents % 25 / 10
+  nickels = cents % 25 % 10 / 5
+  pennies = cents % 25 % 10 % 5 / 1
+  hash = {
+    :quarters => quarters,
+    :dimes => dimes,
+    :nickels => nickels,
+    :pennies => pennies
+  }
 
 end

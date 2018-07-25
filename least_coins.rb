611e922ed5dfa00506e8e0b0b7bb2cd8a643d6b9
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-07-25 19:02:51 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,8 +1,6 @@
 #write out your code here
 
 def least_coins(cents)
-
-#Code your answer here!
 quarters = cents / 25
   dimes = cents % 25 / 10
   nickels = cents % 25 % 10 / 5
@@ -13,5 +11,4 @@ quarters = cents / 25
     :nickels => nickels,
     :pennies => pennies
   }
-
 end
